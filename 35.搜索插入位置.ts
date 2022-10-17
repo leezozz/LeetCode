df6505bbd40b336153ef454f9973b90be36eac5e
gaofuhong <157297759@qq.com>
2022-10-17 09:47:42 +0800

    feat: add complete 搜索插入的位置

--- 35.搜索插入位置.ts
+++ 35.搜索插入位置.ts
@@ -6,10 +6,34 @@
 
 // @lc code=start
 function searchInsert(nums: number[], target: number): number {
-  // 不存在数组中：找到大于他的元素索引，返回该索引；没有大于他的元素，就返回数组的长度（即，数组最后索引+1）
-  var res = nums.findIndex(item => item > target) === -1 ? nums.length : nums.findIndex(item => item > target)
-  // 判断是否存在数组里，存在的话去找到它的索引
-  return nums.includes(target) ? nums.indexOf(target) : res
+  // // 不存在数组中：找到大于他的元素索引，返回该索引；没有大于他的元素，就返回数组的长度（即，数组最后索引+1）
+  // var res = nums.findIndex(item => item > target) === -1 ? nums.length : nums.findIndex(item => item > target)
+  // // 判断是否存在数组里，存在的话去找到它的索引
+  // return nums.includes(target) ? nums.indexOf(target) : res
+
+
+  var res = 0  
+
+  for(var i = 0; i <= nums.length; i++) {
+
+    console.log('---', i)
+
+    if(nums[i] === target) {
+        res = i
+        break
+    }
+
+
+    if(nums[i] > target) {
+        res = i
+        break
+    }
+
+    res = nums.length
+  }
+
+  return res
+
 };
 // @lc code=end
 
